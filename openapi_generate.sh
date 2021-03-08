95020b02d1ee4357976efeb423a27d6476d07a17
Jaime Blasco <git@jaimeblasco.com>
2021-03-08 15:19:20 +0100

    Initial commit

---
+++ openapi_generate.sh
@@ -0,0 +1,7 @@
+# This is not working yet.
+openapi-generator generate -i spec3.yaml -g dart-dio -o stripe_openapi --enable-post-process-file \
+ --reserved-words-mappings=File=STPFile,Source=STPSource  \
+ --import-mappings=File=STPFile,Source=STPSource  \
+ --type-mappings=File=STPFile,Source=STPSource \
+ -c openapi_config.json 
+
