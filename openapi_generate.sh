550c95d362f203ae429531f9c939528471bfad42
Jaime Blasco <git@jaimeblasco.com>
2021-03-08 15:28:01 +0100

    cli Remove folder if previously created

--- openapi_generate.sh
+++ openapi_generate.sh
@@ -1,4 +1,6 @@
 # This is not working yet.
+
+rm -r stripe_openapi
 openapi-generator generate -i spec3.yaml -g dart-dio -o stripe_openapi --enable-post-process-file \
  --reserved-words-mappings=File=STPFile,Source=STPSource  \
  --import-mappings=File=STPFile,Source=STPSource  \
